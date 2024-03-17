7d9d4e02b941bda43dbadae4a43464ace3692a21
Ross <oblivion1214@hotmail.com>
2024-03-17 19:10:13 -0400

    Updated requests handled by server

--- src/SocketHandler.ts
+++ src/SocketHandler.ts
@@ -2,17 +2,9 @@ import { Socket } from 'socket.io';
 import { SocketEvents } from './types';
 import { Message } from './classes';
 
-interface IUserTypingBody {
-    tempUserId: string;
-    isTyping: boolean;
-}
-
 export const socketHandler = (socket: Socket) => {
-    socket.on(SocketEvents.sendMessage, (message: Message) => {
+    socket.on(SocketEvents.sendMessage, (message: Message, callback) => {
         socket.broadcast.emit(SocketEvents.newMessage, message);
-    });
-
-    socket.on(SocketEvents.sendTyping, ({ isTyping }: IUserTypingBody) => {
-        socket.broadcast.emit(SocketEvents.typing, isTyping);
+        callback(message);
     });
 }
--- src/types/SocketEvents.ts
+++ src/types/SocketEvents.ts
@@ -1,6 +1,4 @@
 export enum SocketEvents {
     "sendMessage" = "sendMessage",
-    "sendTyping" = "sendTyping",
-    "typing" = "typing",
     "newMessage" = "newMessage",
 };
