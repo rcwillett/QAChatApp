1b37e0a93a2be408a16d55ce732f66122f7d0025
Ross <oblivion1214@hotmail.com>
2024-03-20 21:05:36 -0400

    Added minor updates to constants and error handling

--- src/SocketHandler.ts
+++ src/SocketHandler.ts
@@ -4,6 +4,7 @@ import { SocketEvents } from './types';
 import { Message, QA } from './classes';
 import { QAService } from './services';
 import { ElasticSearchService, OpenAIService } from './dataServices';
+import { CHATBOT_NAME } from './constants';
 
 export const socketHandler = async (socket: Socket, io: Server, elasticSearch: ElasticSearchService) => {
     socket.on(SocketEvents.sendMessage, async (message: Message, callback) => {
@@ -24,27 +25,19 @@ export const socketHandler = async (socket: Socket, io: Server, elasticSearch: E
     });
 
     const saveAnswer = async (message: Message, questionEmbedding: number[]) => {
-        try {
-            const qaService = new QAService(elasticSearch);
-            await qaService.init();
-            const qa = new QA(message.isReplyTo!.content, message.content, questionEmbedding);
-            await qaService.saveQA(qa);
-        } catch (error) {
-            console.error(error);
-        }
+        const qaService = new QAService(elasticSearch);
+        await qaService.init();
+        const qa = new QA(message.isReplyTo!.content, message.content, questionEmbedding);
+        await qaService.saveQA(qa);
     };
 
     const getBotReply = async (message: Message, questionEmbedding: number[]) => {
-        try {
-            const qaService = new QAService(elasticSearch);
-            await qaService.init();
-            const answer = await qaService.checkForAnswer(questionEmbedding);
-            if (answer) {
-                const botMessage = new Message(uuidv4(), 'CHATBOT', new Date(), answer, message);
-                io.emit(SocketEvents.newMessage, botMessage);
-            }
-        } catch (error) {
-            console.log(error);
+        const qaService = new QAService(elasticSearch);
+        await qaService.init();
+        const answer = await qaService.checkForAnswer(questionEmbedding);
+        if (answer) {
+            const botMessage = new Message(uuidv4(), 'CHATBOT', CHATBOT_NAME, new Date(), answer, message);
+            io.emit(SocketEvents.newMessage, botMessage);
         }
     }
 }
